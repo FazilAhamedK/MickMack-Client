3ca699e081b91aeb466103dce64f90a2489f5495
Fazil Ahamed <seyadian@gmail.com>
2024-11-24 22:50:51 +0530

    chore: redesign look and feel

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,53 +11,50 @@ module.exports =
 		{
       primary: "#0F182C",
       secondary: "#4C421A",
-      neutral: "#FFFFEE",
-      food:
-      {
-        vegan: "#03759B",
-        vegetarian: "#006400",
-        egg: "#A35F1B",
-        nonVegetarian: "#652A03"
-      }
+      neutral: "#FFFFFA",
+      trial: "rgba(241, 139, 105, 0.6)"
     },
     borderColor:
 		{
       primary: "#0F182C",
       secondary: "#4C421A",
-      neutral: "#FFFFEE",
-      food:
-      {
-        vegan: "#03759B",
-        vegetarian: "#006400",
-        egg: "#A35F1B",
-        nonVegetarian: "#652A03"
-      }
+      neutral: "#FFFFFA"
     },
     colors:
 		{
       primary: "#0F182C",
       secondary: "#4C421A",
-      neutral: "#FFFFEE",
-      food:
+      neutral: "#FFFFFA",
+      black:
       {
-        vegan: "#03759B",
-        vegetarian: "#006400",
-        egg: "#A35F1B",
-        nonVegetarian: "#652A03"
+        50: "rgba(0, 0, 0, 0.5)"
       }
+    },
+    fontFamily:
+    {
+      primary: ["Poiret One", "sans-serif"],
+      secondary: ["Pompiere", "sans-serif"]
     },
     textColor:
     {
       primary: "#0F182C",
       secondary: "#4C421A",
-      neutral: "#FFFFEE",
-      food:
+      neutral: "#FFFFFA"
+    },
+    extend:
+    {
+      keyframes:
       {
-        vegan: "#03759B",
-        vegetarian: "#006400",
-        egg: "#A35F1B",
-        nonVegetarian: "#652A03"
-      }
+        marquee:
+        {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation:
+      {
+        marquee: 'marquee 100s linear infinite',
+      },
     }
   },
   plugins: [],
