027e024146526b7594f70005236541d416efe2af
Fazil Ahamed <seyadian@gmail.com>
2024-12-06 01:06:34 +0530

    chore: add menu card template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { StoreComponent } from './store/store.component';
 import { BakeryComponent } from './bakery/bakery.component';
 import { IndulgenceComponent } from './indulgence/indulgence.component';
+import { MenuCardComponent } from './menu-card/menu-card.component';
 
 const routes: Routes =
 [
   {path: "", component: BakeryComponent},
   {path: "indulgence", component: IndulgenceComponent},
   {path: "store", component: StoreComponent},
+  {path: "menu", component: MenuCardComponent},
   {path: "**", redirectTo: "", pathMatch: "full"}
 ];
 
