b1e6b4c1a0adb3cd6cb5f361e35bf2ef5e8ef7bc
Fazil Ahamed <seyadian@gmail.com>
2025-10-16 13:17:28 +0530

    wip: Menu Enhancements
    - add All India Delivery section
    - add minimum order quantity
    - add NYC Cookies to menu

--- src/app/types/menu-item-variant.interface.ts
+++ src/app/types/menu-item-variant.interface.ts
@@ -3,6 +3,7 @@ export interface MenuItemVariant
     skuID: number;
     name: string;
     price: number;
+    minimumOrderQuantity?: number;
     maximumOrderQuantity: number;
     isSoldOut?: boolean;
 }
